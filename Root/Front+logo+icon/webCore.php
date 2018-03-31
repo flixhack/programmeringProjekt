3c6bf2954d81d5bc2df9e05239c014b4701ac6d8
Martin Møldrup Jensen <37298117+HackerboyMartin@users.noreply.github.com>
2018-03-31 19:01:01 +0200

    Iconbar

--- Root/Front+logo+icon/webCore.php
+++ Root/Front+logo+icon/webCore.php
@@ -1,15 +1,49 @@
 <html lang="en" dir="ltr">
-
   <head>
     <meta charset="utf-8">
     <title> Mellow </title>
     <link href="interface.css" rel="stylesheet" type="text/css">
     <link rel="icon" href="icon.png">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
   </head>
 
 <body>
 <img src="Mellow2.png" alt="Title" style="width:190px;height:40px;">
 <p class="sansserif">Hello, welcome to our site. <br /> This is the project for our programming exam.</p>
+<style>
+body {margin:0;}
+
+.icon-bar {
+    width: 100%;
+    background-color: #000;
+    overflow: auto;
+}
+
+.icon-bar a {
+    float: right;
+    width: 10%;
+    text-align: center;
+    padding: 12px 0;
+    transition: all 0.3s ease;
+    color: grey;
+    font-size: 36px;
+}
+
+.icon-bar a:hover {
+    background-color: #000;
+}
+
+.active {
+    background-color: #ffff00 !important;
+}
+</style>
+<body>
+
+<div class="icon-bar">
+  <a class="active" href="#"><i class="fa fa-home"></i></a>
+  <a href="#"><i class="fa fa-search"></i></a>
+  <a href="#"><i class="fa fa-envelope"></i></a>
+</div>
   </body>
 
 </html>
