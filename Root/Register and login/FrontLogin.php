7f8ac7ccc3b8f26a91789ac3930e612d25a3ef01
flixhack <felix.benjamin.mathiassen@gmail.com>
2018-03-31 19:53:50 +0200

    en masse

--- Root/Register and login/FrontLogin.php
+++ Root/Register and login/FrontLogin.php
@@ -1,7 +1,5 @@
 
 <html lang="en" dir="ltr">
- <input type="text" align="middle">
- <input type="image" src="submit.gif" alt="Submit" align="right" width="48" height="48">
   <head>
     <meta charset="utf-8">
     <title> Mellow </title>
@@ -13,5 +11,10 @@
 <img src="IMG/Mellow2.png" alt="Title" style="width:190px;height:40px;">
 <p class="sansserif">Hello, welcome to our site. <br /> This is the project for our programming exam.</p>
   </body>
+
+
+  <input type="password" style="position:absolute;TOP:35px;left:1100px" name="fname"><br>
+  <input type="text" style="position:absolute;TOP:35px;left:970px" name="fname"><br>
+  <input type="image" src="login.png" alt="login" style="position:absolute;TOP:35px;left:1220px;WIDTH:32;HEIGHT:32">
 
 </html>
