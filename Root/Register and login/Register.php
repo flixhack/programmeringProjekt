90560ba3c6a3f6a1d45a0d1e9c2951c2e0751a3a
flixhack <felix.benjamin.mathiassen@gmail.com>
2018-03-31 18:45:15 +0200

    yas

--- Root/Register and login/Register.php
+++ Root/Register and login/Register.php
@@ -1,1 +1,15 @@
-Hej
+
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title> Mellow </title>
+    <link href="interface.css" rel="stylesheet" type="text/css">
+    <link rel="icon" href="IMG/icon.png">
+  </head>
+
+<body>
+<img src="IMG/Mellow2.png" alt="Title" style="width:190px;height:40px;">
+<p class="sansserif">Hello, welcome to our site. <br /> This is the project for our programming exam.</p>
+  </body>
+
+</html>
