8bce5df9d4ac42b9ec732f7dcbdcb0e3eb38b651
Martin Møldrup Jensen <37298117+HackerboyMartin@users.noreply.github.com>
2018-03-31 16:52:48 +0200

    Dunno

---
+++ Root/Front+logo+icon/webCore.php
@@ -0,0 +1,15 @@
+<html lang="en" dir="ltr">
+
+  <head>
+    <meta charset="utf-8">
+    <title> Mellow </title>
+    <link href="interface.css" rel="stylesheet" type="text/css">
+    <link rel="icon" href="icon.png">
+  </head>
+
+<body>
+<img src="Mellow2.png" alt="Title" style="width:190px;height:40px;">
+<p class="sansserif">Hello, welcome to our site. <br /> This is the project for our programming exam.</p>
+  </body>
+
+</html>
