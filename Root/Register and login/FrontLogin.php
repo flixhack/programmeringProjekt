a96709b464328f5a06050e6e29cb0cb83b316e4c
flixhack <felix.benjamin.mathiassen@gmail.com>
2018-04-09 13:06:01 +0200

    n

--- Root/Register and login/FrontLogin.php
+++ Root/Register and login/FrontLogin.php
@@ -16,7 +16,26 @@
 
   <input type="password" style="position:absolute;TOP:35px;left:1030px" placeholder="Password" name="fname"><br>
   <input type="email" style="position:absolute;TOP:35px;left:850px" placeholder="Email" name="fname"><br>
-  <button type="button" style="position:absolute;TOP:35px;left:1210px;WIDTH:60;HEIGHT:22" href="login.php">Login </button>
+  <button type="button" style="position:absolute;TOP:35px;left:1210px;WIDTH:60;HEIGHT:22" onclick="<?php $servername = "localhost";
+  $username = "test";
+  $password = "test";
+
+  // Create connection
+  $conn = mysqli_connect($servername, $username, $password);
+  // Check connection
+  if (!$conn) {
+      die("Connection failed: " . mysqli_connect_error());
+  }
+
+  // Create database
+  $sql = "CREATE DATABASE myDB";
+  if (mysqli_query($conn, $sql)) {
+      echo "Database created successfully";
+  } else {
+      echo "Error creating database: " . mysqli_error($conn);
+  }
+
+  mysqli_close($conn); ?>">Login </button>
 
   <A HREF="Registre.php" style="position:absolute;TOP:60px;left:850px">Not user yet? Sign up here!</A>
 </html>
