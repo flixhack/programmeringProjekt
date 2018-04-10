7c54257dd34e082a74a80fb2388fc96c082cb8e3
flixhack <felix.benjamin.mathiassen@gmail.com>
2018-04-10 13:53:22 +0200

    j

--- Root/Register and login/Registre.php
+++ Root/Register and login/Registre.php
@@ -55,7 +55,7 @@ $password = hash('sha1', $preHashPass);
 
 $userCheck = "SELECT * FROM 'mellowMembers' WHERE email = '$email'";
 $sql = "INSERT INTO 'mellowmembers'('Email', 'Fname', 'Lname', 'Password', 'gender', 'God') VALUES ('$email','$fname','$lname','$password','$gender','f')";
-if (mysqli_query($conn, $sql)){
+if (mysqli_query($conn, $sql)) {
     echo "Database created successfully";
 } else {
     echo "Error creating database: " . mysqli_error($conn);
