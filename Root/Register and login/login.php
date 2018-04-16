0df60c11b73c37656be36002a121f9a573cc8a61
flixhack <felix.benjamin.mathiassen@gmail.com>
2018-04-16 13:57:19 +0200

    jj

--- Root/Register and login/login.php
+++ Root/Register and login/login.php
@@ -3,9 +3,10 @@
 $servername = "localhost";
 $username = "test";
 $password = "test";
+$database = "mellowUsers";
 
 // Create connection
-$conn = mysqli_connect($servername, $username, $password);
+$conn = mysqli_connect($servername, $username, $password, $database);
 // Check connection
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
@@ -19,7 +20,7 @@ $passwordLogin = hash('sha1', $preHashPass);
 
 
 $loginCheck="SELECT * FROM mellowMembers WHERE Email = '{$email}' AND Password = '{$passwordLogin}'";
-rs = mysqli_query($conn,$loginCheck);
+$rs = mysqli_query($conn,$loginCheck);
 if ($rs->num_rows != 0) {
   echo "Logged in";
 } else {
