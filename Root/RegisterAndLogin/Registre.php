0b6e450f246ffe58f9c7d2eea4ffacd9be50cd61
Martin Møldrup Jensen <37298117+HackerboyMartin@users.noreply.github.com>
2018-04-20 12:50:17 +0200

    Øhhh

--- Root/RegisterAndLogin/Registre.php
+++ Root/RegisterAndLogin/Registre.php
@@ -1,3 +1,4 @@
+<<<<<<< current
 
 <html lang="en" dir="ltr">
   <head>
@@ -38,3 +39,45 @@ br>
 
 
 </html>
+=======
+
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title> Mellow </title>
+    <link href="interface.css" rel="stylesheet" type="text/css">
+    <link rel="icon" href="IMG/Icon.png">
+  </head>
+
+<body>
+<img src="IMG/Mellow2.png" alt="Title" style="width:190px;height:40px;">
+<p class="sansserif">Hello, welcome to our site. <br /> This is the project for our programming exam.</p>
+
+<p class="sansserif" style="position:absolute;TOP:240;left:505"> First Name</P>
+<p class="sansserif" style="position:absolute;TOP:265;left:507"> Last Name</P>
+<p class="sansserif" style="position:absolute;TOP:290;left:547"> Email</P>
+<p class="sansserif" style="position:absolute;TOP:315;left:517"> Password</P>
+<p class="sansserif" style="position:absolute;TOP:340;left:448"> Confirm Password</P>
+<p class="sansserif" style="position:absolute;TOP:210;left:730">Female </p>
+<p class="sansserif" style="position:absolute;TOP:210;left:630">Male </p>
+  </body>
+
+<form name="form" action="CreateUserScript.php" method="POST">
+  <input type="text" style="position:absolute;TOP:250px;left:600px;WIDTH:200" name="fname" id="fname"><br>
+  <input type="text" style="position:absolute;TOP:275px;left:600px;WIDTH:200" name="lname" id="lname"><br>
+  <input type="email" style="position:absolute;TOP:300px;left:600px;WIDTH:200" name="email" id="email"><br>
+  <input type="password" style="position:absolute;TOP:325px;left:600px;WIDTH:200" name="pw" id="pw"><br>
+  <input type="password" style="position:absolute;TOP:350px;left:600px;WIDTH:200 " name="cpw" id="cpw"><br>
+  <input type="radio" name="gender" value="m" style="position:absolute;TOP:225px;left:600px"checked id="Male"><
+br>
+  <input type="radio" name="gender" value="f" style="position:absolute;TOP:225px;left:700px" id="Female"> <br><br>
+
+
+<input type="submit" value="Create account" style="position:absolute;TOP:375px;left:600px;WIDTH:200;HEIGHT:20">
+</form>
+
+<A HREF="FrontLogin.php" style="position:absolute;TOP:395px;left:600px">Already a member? Login Here!</A>
+
+
+</html>
+>>>>>>> before discard
