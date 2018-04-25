d3326ba4ffd2789909f46bef5635e8e9ff963272
Martin Møldrup Jensen <37298117+HackerboyMartin@users.noreply.github.com>
2018-04-25 15:00:49 +0200

    Øhhh

--- Root/RegisterAndLogin/CreateUserScript.php
+++ Root/RegisterAndLogin/CreateUserScript.php
@@ -19,6 +19,7 @@ $passwordC = hash('sha1', $preHashPassC);
 $sql = "INSERT INTO mellowMembers VALUES (NULL,'{$email}','{$fname}','{$lname}','{$password}','{$gender}','f')";
 $check="SELECT * FROM mellowMembers WHERE Email = '{$email}'";
 $rs = mysqli_query($conn,$check);
+if ($email != "" && $fname != "" && $lname != "" && $preHashPass != "" && $preHashPassC != "") {
 if ($password == $passwordC) {
   if($rs->num_rows == 0){
     if (mysqli_query($conn, $sql)) {
@@ -29,8 +30,9 @@ if ($password == $passwordC) {
         print '<script type="text/javascript">alert("User already exist"); window.location = "CreateUserScript.php"; </script>';
 } } else {
       print '<script type="text/javascript">alert("Passwords are not identical"); window.location = "Registre.php"; </script>';
+}} else {
+  print '<script type="text/javascript">alert("All fields are MANDATORY!!!"); window.location = "Registre.php"; </script>';
 }
-
 
 mysqli_close($conn);
 
