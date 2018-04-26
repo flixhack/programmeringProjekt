4f45abac79199a812a914c1d3c6b68c4718aa674
Martin Møldrup Jensen <37298117+HackerboyMartin@users.noreply.github.com>
2018-04-26 08:59:22 +0200

    Rettelse

--- Root/Front+logo+icon/Profilepage.php
+++ Root/Front+logo+icon/Profilepage.php
@@ -21,19 +21,6 @@ include 'HTMLIncluder.php';
 
 <?php
 
-
-$servername = "db";
-$username = "root";
-$password = "Dn91ND1aMw3ZkuENdC1j1J8F";
-$database = "MellowUsers";
-
-// Create connection
-$conn = mysqli_connect($servername, $username, $password, $database);
-// Check connection
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-}
-
 $fname = 'Martin Moeldrup';
 $lname = 'Jensen';
 
@@ -43,7 +30,6 @@ $result = mysqli_query($conn, $query);
 
 while($data = mysqli_fetch_array($result)){
   echo $data['fname'] ." ". $data['lname'];
-
 }
 ?>
 
