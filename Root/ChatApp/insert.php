2dfd5dda4712be3de8b67e1374aa52fb117b91d3
Martin Møldrup Jensen <37298117+HackerboyMartin@users.noreply.github.com>
2018-04-29 21:46:08 +0200

    Comment

--- Root/ChatApp/insert.php
+++ Root/ChatApp/insert.php
@@ -5,7 +5,7 @@ $ID = $_SESSION["loggedIn"]; //Sets your Session id to be ID
 
 $qy ="SELECT * FROM mellowMembers WHERE ID = $ID"; //Makes sql query thats searches our database for a member where id is equal to id
 $rs = mysqli_query($conn, $qy); //Connects to database and runs the sql query above
-$data = mysqli_fetch_array($rs); //Fetches the row of the person with the specefik id
+$data = mysqli_fetch_array($rs); //Fetches the row of the person with the specefic id
 $sender = $data['fname']; // Sets sender to be equal the name with correct id from database
 
 
