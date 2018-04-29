855ed56eebe7479da1330730e36f65bb780b97de
flixhack <felix.benjamin.mathiassen@gmail.com>
2018-04-29 20:48:52 +0200

    hh

--- Root/ChatApp/logs.php
+++ Root/ChatApp/logs.php
@@ -1,12 +1,12 @@
 <?php
-include '../Header.php';
+include '../Header.php'; //includes Header.php
 
 $conn = mysqli_connect($servername, $username, $password, $database);
 
-$query ="SELECT * FROM beskeder ORDER BY id DESC";
+$query="SELECT * FROM beskeder ORDER BY id DESC"; // Makes sql query that selects all messages from our database
 
-$result1 = mysqli_query($conn, $query);
+$result1 = mysqli_query($conn, $query); //Connects to database and runs the sql query above
 
-while($extract = mysqli_fetch_array($result1)) {
-	echo "<span>" . $extract['sender'] . "</span>: <span>" . $extract['msg'] . "</span><br />";
+while($extract = mysqli_fetch_array($result1)) { // while loop that connects to database and runs the sql query above
+	echo "<span>" . $extract['sender'] . "</span>: <span>" . $extract['msg'] . "</span><br />"; //Displays the messages on the chat page
 }
