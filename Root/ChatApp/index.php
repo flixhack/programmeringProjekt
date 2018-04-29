5654e942ced66a8f220d2c6b9b92192283ef8162
Martin Møldrup Jensen <37298117+HackerboyMartin@users.noreply.github.com>
2018-04-29 20:36:43 +0200

    Comment

--- Root/ChatApp/index.php
+++ Root/ChatApp/index.php
@@ -15,8 +15,9 @@ include '../Front+logo+icon/HTMLIncluder.php';
 
 <script>
 
+
 function submitChat() {
-	if(form1.msg.value == '') {
+	if(form1.msg.value == '') { //if the textarea is empty, you get message about you need to write a message
 		alert("You need to write a message");
 		return;
 
@@ -31,24 +32,25 @@ function submitChat() {
 		}
 	}
 
-	xmlhttp.open('GET','insert.php?msg='+msg,true);
+	xmlhttp.open('GET','insert.php?msg='+msg,true); //getting the information from insert, which is the users name and message
 	xmlhttp.send();
 
-document.getElementById("output").value='';
+document.getElementById("output").value=''; //Sets the textareas value to nothing after pressing the send button, so the textarea gets cleared
 }
 
 $(document).ready(function(e){
 	$.ajaxSetup({
 		cache: false
 	});
-	setInterval( function(){ $('#chatlogs').load('logs.php'); }, 500 );
+	setInterval( function(){ $('#chatlogs').load('logs.php'); }, 500 ); //Sets the page to refresh and load logs every 0.5 s
 });
 
 </script>
 
-
 </head>
+
 <body>
+<!--Creating icon bar-->
 <div class="icon-bar">
   <a href="../Front+logo+icon/Profilepage.php"><i class="fa fa-user"></i></a>
   <a class="active" href="../ChatApp/#"><i class="fa fa-envelope"></i></a>
@@ -56,7 +58,9 @@ $(document).ready(function(e){
   <a href="../Front+logo+icon/webCore.php"><i class="fa fa-home"></i></a>
   <a href="../destroySession.php"><i class="fa fa-sign-out"></i></a>
 </div>
+
 <h1>Chat</h1>
+
 <form>
 <input type="text" placeholder="Search..." style="position:absolute;TOP:15px;left:1040px;WIDTH:150;HEIGHT:21" name="search"><br>
 </form>
@@ -65,8 +69,9 @@ $(document).ready(function(e){
 <form name="form1">
 Your Message: <br />
 <textarea id='output' name="msg" style="color: yellow; background-color: #2f2f2f" rows=5 cols=30></textarea><br />
-<input id="Send" type="button" class="button" value="Send" onclick="submitChat();" />
+<input id="Send" type="button" class="button" value="Send" onclick="submitChat();" /> <!--A button which runs the submitChat function when pressed. This function uses  XMLHttpRequest (XHR) objects to interact with our server, so we can retrieve data from a URL without having to do a full page refres -->
 </form>
+
 <div id="chatlogs">
 LOADING CHAT...
 </div>
